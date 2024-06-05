6419b5fb3e3b71f504a42d5643d19c0c00b32678
Hariz-Ade14 <sannihariz14@gmail.com>
2024-06-04 21:51:22 -0700

    completed the body section,getting started with the footer section

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -3,8 +3,13 @@ import "./Body.css";
 import logo from "../../assets/download.jpeg";
 import appStore from "../../assets/appstore.jpg";
 import playStore from "../../assets/googleplay.jpg";
-import card from "../../assets/debitcard.jpeg";
-import {FaCreditCard} from "react-icons/fa";
+import kuda from "../../assets/kuda.png";
+import hello from "../../assets/hello.png";
+import coinjar from "../../assets/coinjar.png";
+import debitcard from "../../assets/debit-card.avif";
+import acct from "../../assets/acct.jpeg";
+import card from "../../assets/makers_kb2-scaled.jpg";
+import {FaCreditCard,FaArrowRight} from "react-icons/fa";
 
 
 function Body() {
@@ -44,16 +49,89 @@ function Body() {
                </div>
           </div>
 
-          <div className="content-2 row my-5 mx-3 d-flex align-items-center justify-content-center">
-               <div className='col-12 col-sm-6 p-5'>
+          <div className="content-2 row mx-3 d-flex align-items-center justify-content-center">
+               <div className='col-12 col-sm-5 p-5'>
                    <h1>
                     Your phone + <br/>our app + <br/>a debit card = <br/> a simpler life.
                    </h1>
-                   <p>We designed a money app for your lifestyle, and you can get a debit card to go with it. That's all you need to make the right money moves.</p>
+                   <p className='details'>We designed a money app for your lifestyle, and you can get a debit card to go with it. That's all you need to make the right money moves.</p>
+                   
+                   <p>
+                     <a href='/' className='link'>Open an account in minutes<FaArrowRight className='mx-2'/></a>
+                   </p>
                </div>
-               <img src={card} width className='col-12 col-sm-6 p-0 bg-danger m-0' height={"500px"} alt="logo" />
-
+               <img src={debitcard} width className='col-12 col-sm-5 p-0 bg-danger m-0' height={"500px"} alt="logo" />
           </div>
+
+
+          <div className="content-2 row mx-3 d-flex align-items-center justify-content-center">        
+               <img src={kuda} width className='col-12 col-sm-5 p-0 bg-danger m-0' height={"500px"} alt="logo" />
+               <div className='col-12 col-sm-5 p-5'>
+                   <h1>
+                       It’s your money, we just help you manage it.
+                   </h1>
+                   <p>Save it, spend it, send it. It’s up to you. Whatever you choose to do with your money, we’ll make sure it’s done better and free of charge. We take responsibility for that.</p>
+                   
+                   {/* <p>
+                     <a href='/' className='link'>Open an account in minutes<FaArrowRight className='mx-2'/></a>
+                   </p> */}
+               </div>
+          </div>
+          <div className="content-2 row mx-3 d-flex align-items-center justify-content-center">        
+               <div className='col-12 col-sm-5 p-5'>
+                   <h1>
+                   Save money as you spend it, seriously.
+                   </h1>
+                   <p>You can’t avoid spending. That’s how you pay for your needs. But we can help you put money away every time you pay for something. Just set a percentage to save and watch your money grow.</p>
+                   
+                   <p>
+                     <a href='/' className='link'>See all your savings<FaArrowRight className='mx-2'/></a>
+                   </p>
+               </div>
+               <img src={acct} width className='col-12 col-sm-5 p-0 bg-danger m-0' height={"500px"} alt="logo" />
+          </div>
+          <div className="content-2 row mx-3 d-flex align-items-center justify-content-center">        
+               <img src={card} width className='col-12 col-sm-5 p-0 bg-danger m-0' height={"500px"} alt="logo" />
+               
+               <div className='col-12 col-sm-5 p-5'>
+                   <h1>
+                   Turn off access, turn on safety.
+                   </h1>
+                   <p>Life happens. Milk spills. Debit cards go missing. If that ever happens, you can block your missing card on the app so no one can use it. We’d like to see them try..</p>
+                   
+                   <p>
+                     <a href='/' className='link'>Learn more about cards<FaArrowRight className='mx-2'/></a>
+                   </p>
+               </div>
+          </div>
+          <div className="content-2 row mx-3 d-flex align-items-center justify-content-center">        
+               <div className='col-12 col-sm-5 p-5'>
+                   <h1>
+                     We’re always happy to help you.
+                   </h1>
+                   <p>You can chat with us on the app, slide into our DMs, tweet, leave an Instagram comment, send an email or call. However you choose to reach out, there’ll always be a friendly person there to make your life easy.</p>
+                   
+                   <p>
+                     <a href='/' className='link'>Get help<FaArrowRight className='mx-2'/></a>
+                   </p>
+               </div>
+               <img src={hello} width className='col-12 col-sm-5 p-0 bg-danger m-0' height={"400px"} alt="logo" />
+          </div>
+          <div className="content-2 row mb-5 mx-3 d-flex align-items-center justify-content-center">        
+            <img src={coinjar} className='coinjar col-12 col-sm-5 mx-5 p-5 m-0' alt="logo" />
+               <div className='col-12 col-sm-5 mx-5 p-5'>
+                   <h1>
+                   Fees as clear as glass.
+                   </h1>
+                   <p>We’re serious about free banking, and we will never, ever charge you for anything without your consent.</p>
+                   
+                   <p>
+                     <a href='/' className='link'>See all your fees<FaArrowRight className='mx-2'/></a>
+                   </p>
+               </div>
+          </div>
+
+          <h2 className='text-center' style={{fontWeight: "bold",color: "rgb(72, 3, 104)"}}>Don’t just take our word for it</h2>
     </div>
   )
 }
